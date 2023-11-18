edff6caa6e901e4bb987a86888517ce500eeb629
tejas holla <tejasholla17@gmail.com>
2023-11-18 20:40:19 +0530

    commit deleted files

--- pdf/pdfMerger.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2
-import os
-
-merger = PyPDF2.PdfFileMerger()
-
-for file in os.listdir(os.curdir):
-    if file.endswith(".pdf"):
-        merger.append(file)
-merger.write("combined.pdf")
