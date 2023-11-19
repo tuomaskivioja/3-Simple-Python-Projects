1649ae01f927fe5b1b1b72ce94b00cb86b73b3aa
tejas holla <tejasholla17@gmail.com>
2023-11-19 13:35:55 +0530

    new update

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,60 +1,54 @@
-from pytube import YouTube
-from pytube import Playlist
+from pytube import YouTube, Playlist
 from tqdm import tqdm
-import pywhatkit as kit
-from youtubesearchpython import VideosSearch
-from tkinter import *
-import os 
+import os
 
-try:
-    checkifurl = input("Do you have url ( y or n ) : ")
-    if checkifurl == "y":
-        # Ask the user to input the YouTube URL
-        url = input("Enter the youtube URL to download : ")
+def progress_function(stream, chunk, bytes_remaining):
+    current = ((stream.filesize - bytes_remaining) / stream.filesize)
+    percent = ('{0:.1f}').format(current * 100)
+    progress = int(50 * current)
+    status = '█' * progress + '-' * (50 - progress)
+    print(f'\r|{status}| {percent}%', end='')
+
+def update_playlist_progress(completed, total):
+    percent = (completed / total) * 100
+    print(f"\nPlaylist Download Progress: {completed}/{total} videos ({percent:.1f}%)")
+
+def download_video(url, path, audio_only=False):
+    yt = YouTube(url)
+    yt.register_on_progress_callback(progress_function)
+    stream = yt.streams.filter(only_audio=audio_only).first()
+    print(f"Downloading: {yt.title}")
+    stream.download(output_path=path)
+    print(f"\nDownload complete: {yt.title}")
+
+def download_playlist(url, directory, audio_only=False):
+    playlist = Playlist(url)
+    path = os.path.join("D:/Videos/Playlists", directory)
+    os.makedirs(path, exist_ok=True)
+    total_videos = len(playlist.video_urls)
+    completed_videos = 0
+
+    print(f"Downloading playlist: {playlist.title}")
+    for video in playlist.videos:
+        download_video(video.watch_url, path, audio_only)
+        completed_videos += 1
+        update_playlist_progress(completed_videos, total_videos)
+    print(f"Playlist download complete: {playlist.title}")
+
+def main():
+    url = input("Enter the YouTube URL: ")
+    audio_or_video = input("Download audio only? (y/n): ").lower() == 'y'
+
+    if 'playlist?list' in url:
+        directory = input("Enter Playlist folder name: ")
+        download_playlist(url, directory, audio_or_video)
     else:
-        # Ask the user to input the YouTube video
-        ytsearchres = input("What do you want to search : ")
-        kit.playonyt(ytsearchres)
-        #search = VideosSearch(ytsearchres,limit = 5)
-        #print(search.result()['result'])
-        # Ask the user to input the YouTube URL
-        url = input("Enter the youtube URL to download : ")    
+        path = 'D:/Music' if audio_or_video else 'D:/Videos'
+        download_video(url, path, audio_or_video)
 
-    # Ask the user to input if URL is playlist or not
-    #videotype = input("Is it a playlist( y or n ): ")
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-    audioorvideo = input("Do you want only audio( y or n ): ")
-    #if videotype == "y":
-    if "playlist?list" in url:
-        directory = input("Enter Playlist folder name : ")
-        parent_dir = "D:/Videos/Playlist"
-
-        path = os.path.join(parent_dir, directory) 
-        try:
-            os.mkdir(path)  
-            print("Playlist folder '% s' created" % directory) 
-        except OSError as error: 
-            print(error)
-        yt = Playlist(url)
-
-        print("Downloading:", yt.title)
-        if audioorvideo == "y":
-            for video in tqdm(yt.videos):
-                video.streams.filter(only_audio=True).first().download(path) # Downloader for audio
-        else:
-            for video in tqdm(yt.videos):
-                video.streams.get_highest_resolution().download(path) # Downloader for video
-    else:
-        yt = YouTube(url)
-
-        print("Downloading:", yt.title)
-        if audioorvideo == "y":
-            # Get the highest resolution stream
-            yd = yt.streams.filter(only_audio=True).first().download('D:/Music') # Downloader for audio
-        else:   
-            # Get the highest resolution stream
-            yd = yt.streams.get_highest_resolution().download('D:/Videos') # Downloader for video
-
-    print("Download complete.")
-except Exception as e:
-    print("An error occurred:", str(e))
