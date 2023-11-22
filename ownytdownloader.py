03db76da54acece07b3759159302805655b44827
tejasholla <tejasholla17@gmail.com>
2023-11-22 12:33:03 +0530

    Add file integrity check and file extension correction

--- ownytdownloader.py
+++ ownytdownloader.py
@@ -9,6 +9,28 @@ def sanitize_filename(title):
         title = title.replace(char, '')
     return title
 
+def check_file_integrity(file_path, ffmpeg_path='C:\\ffmpeg\\bin\\ffmpeg.exe'):
+    try:
+        print(f"\nChecking file integrity for {file_path}...")
+        result = subprocess.run(
+            [ffmpeg_path, '-v', 'error', '-i', file_path, '-f', 'null', '-'],
+            check=True, text=True, stderr=subprocess.PIPE
+        )
+        if result.stderr:
+            print(f"\nFile {file_path} might be corrupted. Errors: {result.stderr}")
+        else:
+            print(f"\nFile {file_path} is not corrupted.")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while checking {file_path}: {e.stderr}")
+
+def correct_file_extension(file_path, desired_extension):
+    if not os.path.splitext(file_path)[1]:  # No extension
+        new_file_path = f"{file_path}.{desired_extension}"
+        os.rename(file_path, new_file_path)
+        print(f"File renamed to {new_file_path}")
+        return new_file_path
+    return file_path
+
 def download_video(yt, path):
     stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
     tqdm_instance = tqdm(total=stream.filesize, unit='B', unit_scale=True, desc='Downloading', ascii=True)
@@ -20,18 +42,30 @@ def download_video(yt, path):
     yt.register_on_progress_callback(progress_function)
 
     sanitized_title = sanitize_filename(yt.title)
+    file_path = os.path.join(path, sanitized_title)
     stream.download(output_path=path, filename=sanitized_title)
     tqdm_instance.close()
     print(f"\nDownloaded {sanitized_title} to {path}")
+
+    # Correct file extension if necessary
+    file_path = correct_file_extension(file_path, "mp4")
+
+    # Check file integrity
+    check_file_integrity(file_path)
     
 def download_audio(yt, path):
     stream = yt.streams.filter(only_audio=True, file_extension='mp3').order_by('abr').desc().first()
 
+    sanitized_title = sanitize_filename(yt.title)
+    final_filename = f"{sanitized_title}.mp3"
+    file_path = os.path.join(path, final_filename)
+
     if not stream:
         print("MP3 format not available. Downloading in best available audio format and converting to MP3.")
+        # Select the best audio stream regardless of format
         stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
-        temp_filename = sanitize_filename(yt.title) + "." + stream.mime_type.split('/')[1]
-        final_filename = sanitize_filename(yt.title) + ".mp3"
+        temp_filename = f"{sanitized_title}.{stream.mime_type.split('/')[1]}"
+        temp_file_path = os.path.join(path, temp_filename)
 
         tqdm_instance = tqdm(total=stream.filesize, unit='B', unit_scale=True, desc='Downloading', ascii=True)
 
@@ -43,10 +77,10 @@ def download_audio(yt, path):
         stream.download(output_path=path, filename=temp_filename)
         tqdm_instance.close()
 
-        # Specify the full path to your FFmpeg executable here
+        # Convert to MP3
         ffmpeg_path = 'C:\\ffmpeg\\bin\\ffmpeg.exe'
-        subprocess.run([ffmpeg_path, '-i', os.path.join(path, temp_filename), os.path.join(path, final_filename)])
-        os.remove(os.path.join(path, temp_filename))
+        subprocess.run([ffmpeg_path, '-i', temp_file_path, file_path])
+        os.remove(temp_file_path)
         print(f"\nConverted to MP3 and saved as {final_filename} in {path}")
     else:
         tqdm_instance = tqdm(total=stream.filesize, unit='B', unit_scale=True, desc='Downloading', ascii=True)
@@ -56,12 +90,17 @@ def download_audio(yt, path):
             tqdm_instance.update(current - tqdm_instance.n)
 
         yt.register_on_progress_callback(progress_function)
-        sanitized_title = sanitize_filename(yt.title) + ".mp3"
-        stream.download(output_path=path, filename=sanitized_title)
+        stream.download(output_path=path, filename=final_filename)
         tqdm_instance.close()
-        print(f"\nDownloaded {sanitized_title} to {path}")
+        print(f"\nDownloaded {final_filename} to {path}")
 
-def download_playlist(url, path):
+    # Correct file extension if necessary
+    file_path = correct_file_extension(file_path, "mp3")
+
+    # Check file integrity
+    check_file_integrity(file_path)
+
+def download_playlist(url, path, download_choice):
     pl = Playlist(url)
     print(f"\nPlaylist details: ")
     print(f"Playlist name: {pl.title}")
@@ -69,8 +108,11 @@ def download_playlist(url, path):
 
     for index, video_url in enumerate(pl.video_urls, start=1):
         yt = YouTube(video_url)
-        print(f"\nDownloading video {index} of {len(pl.video_urls)}: {yt.title}")
-        download_video(yt, path)
+        print(f"\nDownloading {('audio' if download_choice == 'a' else 'video')} {index} of {len(pl.video_urls)}: {yt.title}")
+        if download_choice == 'v':
+            download_video(yt, path)
+        elif download_choice == 'a':
+            download_audio(yt, path)
 
 def main():
     url = input("Enter the YouTube URL: ")
@@ -81,7 +123,7 @@ def main():
         playlist_path = input("\nEnter the folder name for the playlist: ")
         path = os.path.join("D:/Videos/Playlists", playlist_path)
         os.makedirs(path, exist_ok=True)
-        download_playlist(url, path)
+        download_playlist(url, path, download_choice)  # Pass download_choice here
     else:
         yt = YouTube(url)
         if download_choice == 'v':
--- ytDownloader.py
+++ ytDownloader.py
@@ -1,53 +1,118 @@
 from pytube import YouTube, Playlist
 import os
+import subprocess
 from tqdm import tqdm
+
+def sanitize_filename(title):
+    invalid_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
+    for char in invalid_chars:
+        title = title.replace(char, '')
+    return title
+
+def check_file_integrity(file_path, ffmpeg_path='C:\\ffmpeg\\bin\\ffmpeg.exe'):
+    try:
+        print(f"\nChecking file integrity for {file_path}...")
+        result = subprocess.run(
+            [ffmpeg_path, '-v', 'error', '-i', file_path, '-f', 'null', '-'],
+            check=True, text=True, stderr=subprocess.PIPE
+        )
+        if result.stderr:
+            print(f"\nFile {file_path} might be corrupted. Errors: {result.stderr}")
+        else:
+            print(f"\nFile {file_path} is not corrupted.")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while checking {file_path}: {e.stderr}")
+
+def correct_file_extension(file_path, desired_extension):
+    if not os.path.splitext(file_path)[1]:  # No extension
+        new_file_path = f"{file_path}.{desired_extension}"
+        os.rename(file_path, new_file_path)
+        print(f"File renamed to {new_file_path}")
+        return new_file_path
+    return file_path
 
 def download_video(yt, path):
     stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-    
-    # Define the tqdm progress bar
     tqdm_instance = tqdm(total=stream.filesize, unit='B', unit_scale=True, desc='Downloading', ascii=True)
 
     def progress_function(stream, chunk, bytes_remaining):
         current = stream.filesize - bytes_remaining
-        tqdm_instance.update(current - tqdm_instance.n)  # update tqdm instance with downloaded bytes
+        tqdm_instance.update(current - tqdm_instance.n)
 
     yt.register_on_progress_callback(progress_function)
-    stream.download(output_path=path)
-    tqdm_instance.close()
-    print(f"\nDownloaded {yt.title} to {path}")
 
+    sanitized_title = sanitize_filename(yt.title)
+    file_path = os.path.join(path, sanitized_title)
+    stream.download(output_path=path, filename=sanitized_title)
+    tqdm_instance.close()
+    print(f"\nDownloaded {sanitized_title} to {path}")
+
+    # Correct file extension if necessary
+    file_path = correct_file_extension(file_path, "mp4")
+
+    # Check file integrity
+    check_file_integrity(file_path)
+    
 def download_audio(yt, path):
-    # Attempt to find an audio stream in mp3 format
     stream = yt.streams.filter(only_audio=True, file_extension='mp3').order_by('abr').desc().first()
 
-    # If no mp3 stream is available, default to the best audio format available
+    sanitized_title = sanitize_filename(yt.title)
+    final_filename = f"{sanitized_title}.mp3"
+    file_path = os.path.join(path, final_filename)
+
     if not stream:
-        print("MP3 format not available. Downloading in best available audio format.")
+        print("MP3 format not available. Downloading in best available audio format and converting to MP3.")
+        # Select the best audio stream regardless of format
         stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
+        temp_filename = f"{sanitized_title}.{stream.mime_type.split('/')[1]}"
+        temp_file_path = os.path.join(path, temp_filename)
 
-    tqdm_instance = tqdm(total=stream.filesize, unit='B', unit_scale=True, desc='Downloading', ascii=True)
+        tqdm_instance = tqdm(total=stream.filesize, unit='B', unit_scale=True, desc='Downloading', ascii=True)
 
-    def progress_function(stream, chunk, bytes_remaining):
-        current = stream.filesize - bytes_remaining
-        tqdm_instance.update(current - tqdm_instance.n)  # update tqdm instance with downloaded bytes
+        def progress_function(stream, chunk, bytes_remaining):
+            current = stream.filesize - bytes_remaining
+            tqdm_instance.update(current - tqdm_instance.n)
 
-    yt.register_on_progress_callback(progress_function)
-    stream.download(output_path=path, filename=f"{yt.title}.mp3")  # filename always set to .mp3 extension
-    tqdm_instance.close()
-    print(f"\nDownloaded {yt.title} to {path}")
+        yt.register_on_progress_callback(progress_function)
+        stream.download(output_path=path, filename=temp_filename)
+        tqdm_instance.close()
 
-def download_playlist(url, path):
+        # Convert to MP3
+        ffmpeg_path = 'C:\\ffmpeg\\bin\\ffmpeg.exe'
+        subprocess.run([ffmpeg_path, '-i', temp_file_path, file_path])
+        os.remove(temp_file_path)
+        print(f"\nConverted to MP3 and saved as {final_filename} in {path}")
+    else:
+        tqdm_instance = tqdm(total=stream.filesize, unit='B', unit_scale=True, desc='Downloading', ascii=True)
+
+        def progress_function(stream, chunk, bytes_remaining):
+            current = stream.filesize - bytes_remaining
+            tqdm_instance.update(current - tqdm_instance.n)
+
+        yt.register_on_progress_callback(progress_function)
+        stream.download(output_path=path, filename=final_filename)
+        tqdm_instance.close()
+        print(f"\nDownloaded {final_filename} to {path}")
+
+    # Correct file extension if necessary
+    file_path = correct_file_extension(file_path, "mp3")
+
+    # Check file integrity
+    check_file_integrity(file_path)
+
+def download_playlist(url, path, download_choice):
     pl = Playlist(url)
-    # Playlist details 
     print(f"\nPlaylist details: ")
     print(f"Playlist name: {pl.title}")
     print(f"Total videos in the playlist: {len(pl.video_urls)}")
 
     for index, video_url in enumerate(pl.video_urls, start=1):
         yt = YouTube(video_url)
-        print(f"\nDownloading video {index} of {len(pl.video_urls)}: {yt.title}")
-        download_video(yt, path)
+        print(f"\nDownloading {('audio' if download_choice == 'a' else 'video')} {index} of {len(pl.video_urls)}: {yt.title}")
+        if download_choice == 'v':
+            download_video(yt, path)
+        elif download_choice == 'a':
+            download_audio(yt, path)
 
 def main():
     url = input("Enter the YouTube URL: ")
@@ -58,7 +123,7 @@ def main():
         playlist_path = input("\nEnter the folder name for the playlist: ")
         path = os.path.join("D:/Videos/Playlists", playlist_path)
         os.makedirs(path, exist_ok=True)
-        download_playlist(url, path)
+        download_playlist(url, path, download_choice)  # Pass download_choice here
     else:
         yt = YouTube(url)
         if download_choice == 'v':
