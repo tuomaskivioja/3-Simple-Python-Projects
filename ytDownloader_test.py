5360815d06cfbaa00d568b149e661d174ddc4407
tejas holla <tejasholla17@gmail.com>
2023-11-27 20:48:28 +0530

    Add command-line argument parsing for advanced users

--- ytDownloader_test.py
+++ ytDownloader_test.py
@@ -9,6 +9,7 @@ import time
 import logging
 from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3
+import argparse
 
 # Constants for retry mechanism
 MAX_RETRIES = 3
@@ -374,50 +375,98 @@ def select_videos(playlist):
     selected_indices = [int(i) - 1 for i in selections.split(',') if i.isdigit()]
     return [playlist.video_urls[i] for i in selected_indices if i < len(playlist.video_urls)]
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="YouTube Video and Audio Downloader")
+    
+    parser.add_argument("-m", "--mode", choices=['single', 'batch'], help="Download mode: single or batch")
+    parser.add_argument("-t", "--type", choices=['video', 'audio'], help="Download type: video or audio")
+    parser.add_argument("-u", "--urls", nargs='+', help="List of URLs to download for batch mode")
+    parser.add_argument("-f", "--file", type=str, help="File path containing URLs for batch mode")
+    parser.add_argument("-d", "--directory", type=str, help="Download directory")
+    parser.add_argument("-p", "--playlist", action="store_true", help="Indicates the URL is a playlist")
+    parser.add_argument("-plc", "--playlist_choice", choices=['e', 's'], help="Download entire playlist or select specific videos (e/s)")
+
+    return parser.parse_args()
+
 # Main function to handle user input and start the download process
 def main():
     try:
+        args = parse_arguments()
+
         log_dir = get_default_directory('log')
-        download_dir = get_default_directory('download')
-        url = None  # Initialize url to None
-        download_mode = input("Enter 'S' for single download or 'B' for batch download: ").lower()
-        download_choice = input("Would you like to download Video or Audio? Please enter 'V' for Video or 'A' for Audio: ").lower()
-        if download_mode == 'b':
-            batch_mode = input("Enter 'U' to input URLs or 'F' to read from a file: ").lower()
-            if batch_mode == 'u':
-                urls = input("Enter video URLs separated by commas: ").split(',')
-            elif batch_mode == 'f':
-                file_path = input("Enter the file path containing video URLs: ")
-                with open(file_path, 'r') as file:
+        download_dir = args.directory if args.directory else get_default_directory('download')
+
+        if args.mode == 'batch':
+            if args.urls:
+                urls = args.urls
+            elif args.file:
+                with open(args.file, 'r') as file:
                     urls = file.read().splitlines()
             else:
-                raise ValueError("Invalid batch mode selected")
-
-            path = input("Enter the download directory: ")
-            os.makedirs(path, exist_ok=True)
-            batch_download(urls, path, download_choice, log_dir)
-        else:
-            url = input("Please enter the full YouTube URL (video or playlist): ")
-            is_playlist = 'playlist' in url
-            print(f"Default log file directory is '{log_dir}'.")
-
-            # Handling downloads for playlists and individual videos or audio
-            if is_playlist:
-                playlist_choice = input("Do you want to download the entire playlist or select specific videos? Enter 'E' for entire or 'S' for select: ").lower()
-                playlist_path = input("\nEnter the folder name for the playlist: ")
-                path = os.path.join(download_dir, "/Playlists", playlist_path)
-                os.makedirs(path, exist_ok=True)
-                download_playlist(url, path, download_choice, log_dir, playlist_choice)  # Pass download_choice here
+                raise ValueError("URLs or a file path is required for batch download")
+            os.makedirs(download_dir, exist_ok=True)
+            batch_download(urls, download_dir, args.type, log_dir)
+        elif args.mode == 'single':
+            if not args.urls:
+                raise ValueError("URL is required for single download")
+            url = args.urls[0]
+            if args.playlist:
+                playlist_path = os.path.join(download_dir, "Playlists")
+                os.makedirs(playlist_path, exist_ok=True)
+                download_playlist(url, playlist_path, args.type, log_dir, args.playlist_choice)
             else:
-                yt = YouTube(url)
-                if download_choice == 'v':
-                    video_path = os.path.join(download_dir, "/Videos")
+                if args.type == 'video':
+                    video_path = os.path.join(download_dir, "Videos")
                     os.makedirs(video_path, exist_ok=True)
+                    yt = YouTube(url)
                     download_highest_quality_video(yt, video_path, log_dir)
-                elif download_choice == 'a':
-                    audio_path = os.path.join(download_dir, "/Music")
+                elif args.type == 'audio':
+                    audio_path = os.path.join(download_dir, "Music")
                     os.makedirs(audio_path, exist_ok=True)
+                    yt = YouTube(url)
                     download_audio(yt, audio_path, log_dir)
+        else:
+            log_dir = get_default_directory('log')
+            download_dir = get_default_directory('download')
+            url = None  # Initialize url to None
+            download_mode = input("Enter 'S' for single download or 'B' for batch download: ").lower()
+            download_choice = input("Would you like to download Video or Audio? Please enter 'V' for Video or 'A' for Audio: ").lower()
+            if download_mode == 'b':
+                batch_mode = input("Enter 'U' to input URLs or 'F' to read from a file: ").lower()
+                if batch_mode == 'u':
+                    urls = input("Enter video URLs separated by commas: ").split(',')
+                elif batch_mode == 'f':
+                    file_path = input("Enter the file path containing video URLs: ")
+                    with open(file_path, 'r') as file:
+                        urls = file.read().splitlines()
+                else:
+                    raise ValueError("Invalid batch mode selected")
+
+                path = input("Enter the download directory: ")
+                os.makedirs(path, exist_ok=True)
+                batch_download(urls, path, download_choice, log_dir)
+            else:
+                url = input("Please enter the full YouTube URL (video or playlist): ")
+                is_playlist = 'playlist' in url
+                print(f"Default log file directory is '{log_dir}'.")
+
+                # Handling downloads for playlists and individual videos or audio
+                if is_playlist:
+                    playlist_choice = input("Do you want to download the entire playlist or select specific videos? Enter 'E' for entire or 'S' for select: ").lower()
+                    playlist_path = input("\nEnter the folder name for the playlist: ")
+                    path = os.path.join(download_dir, "/Playlists", playlist_path)
+                    os.makedirs(path, exist_ok=True)
+                    download_playlist(url, path, download_choice, log_dir, playlist_choice)  # Pass download_choice here
+                else:
+                    yt = YouTube(url)
+                    if download_choice == 'v':
+                        video_path = os.path.join(download_dir, "/Videos")
+                        os.makedirs(video_path, exist_ok=True)
+                        download_highest_quality_video(yt, video_path, log_dir)
+                    elif download_choice == 'a':
+                        audio_path = os.path.join(download_dir, "/Music")
+                        os.makedirs(audio_path, exist_ok=True)
+                        download_audio(yt, audio_path, log_dir)
     except ValueError as e:
         print(f"Invalid input: {e}")
         log_download_details(url, "Failed", log_dir, str(e))
