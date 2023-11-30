7ba73ee099f3d34979be97543706eab6bc31d0c0
tejasholla <tejasholla17@gmail.com>
2023-11-30 12:42:18 +0530

    Update CURRENT_VERSION to 1.0.2

--- ytDownloader_test.py
+++ ytDownloader_test.py
@@ -16,7 +16,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-CURRENT_VERSION = "1.0.1"
+CURRENT_VERSION = "1.0.2"
 
 analytics_file = "download_analytics.json"
 
